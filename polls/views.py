dbc1ef8cc3876152c12632721adb31f955f53122
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-12-06 19:56:54 -0300

    Initial commit

---
+++ polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# function-based view
+def index(request):
+    return HttpResponse("Olá mundo! Página index do polls app.")
