b7f38a3b1177a2949fed7af95657f9512e0f7431
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-12-07 22:12:31 -0300

    commit

--- polls/urls.py
+++ polls/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 
 # import relativo
 # A partir da pasta onde o arquivo está, import o módulo views
-from . import views
+from .views import index, detail, results, vote
+
+# namespace
+app_name = "polls"
 
 urlpatterns = [
+    path("", index, name="index"),
     # question_id é um parâmetro passado via URL
     # ou seja, ele pode mudar a qualquer chamado
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote")
+    path("<int:question_id>/", detail, name="detail"),
+    path("<int:question_id>/results/", results, name="results"),
+    path("<int:question_id>/vote/", vote, name="vote")
 ]
 
